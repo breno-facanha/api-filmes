1ddf052c21248259363bb9bd1bf97073c1a533ce
Breno Façanha <brenofacanha.ti@gmail.com>
2025-04-22 21:42:43 -0300

    criado insertFilmes

--- src/controllers/filmes.controller.js
+++ src/controllers/filmes.controller.js
@@ -1,9 +1,26 @@
+const connection = require('../models/connection')
+
 function getFilmes(req, res){
     res.send("Rota de Filmes")
 }
 
-function insertFilmes(req, res){
+async function insertFilmes(req, res){
+    const { titulo, genero, ano, minutos } = req.body
 
+    if(!titulo, !genero, !ano, !minutos){
+        res.status(400).send({message:  "Inserir dados corretamente"})
+    }
+    
+    try {
+        const filme = await connection.query( `INSERT INTO filmes (titulo, genero, ano, minutos)
+        VALUES ('${titulo}', '${genero}', ${ano}, ${minutos})
+        RETURNING *
+        `)
+
+        return res.status(201).send(filme.rows[0])
+    } catch (error) {
+        return res.status(400).send(error.message)
+    }
 }
 
 module.exports = {
--- src/routes/filmes.js
+++ src/routes/filmes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controllerFilmes = require('../controllers/filmes.controller.js')
 
 router.get('/', controllerFilmes.getFilmes)
+router.post('/', controllerFilmes.insertFilmes)
 
 module.exports = router
